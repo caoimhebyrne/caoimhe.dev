1cee374f93d14be84f079ee2359b83d8547a7bd6
Caoimhe <caoimhebyrne06@gmail.com>
2023-10-15 13:16:04 +0100

    Index: Add a basic introduction

---
+++ src/lib/components/typography/InlineLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let href: string;
+  export let external: boolean = false;
+</script>
+
+<a
+  class="text-blue-600 transition-colors hover:text-blue-400 dark:text-blue-500 dark:hover:text-blue-300"
+  rel="noreferrer noopener"
+  target={external ? "_blank" : null}
+  {href}
+>
+  <slot />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
+  import InlineLink from "$lib/components/typography/InlineLink.svelte";
   import Title from "$lib/components/typography/Title.svelte";
 </script>
 
-<Title>Hi, I'm Caoimhe 👋</Title>
+<section class="flex flex-col gap-2">
+  <Title>Hi, I'm Caoimhe 👋</Title>
+
+  <h2 class="text-lg text-neutral-700 dark:text-neutral-300">
+    I'm a software developer and open-source contributor. I currently work at
+    <InlineLink href="https://essential.gg" external>Essential</InlineLink>
+    and code in Java, Kotlin, and TypeScript.
+  </h2>
+</section>
