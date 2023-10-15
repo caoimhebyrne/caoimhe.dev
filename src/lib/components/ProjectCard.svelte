d392d693a064d0259851a0ff5711ac33ab64beac
Caoimhe <caoimhebyrne06@gmail.com>
2023-10-15 14:29:50 +0100

    Index: Add Essential project card

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import ArrowUpRight from "./icons/ArrowUpRight.svelte";
+
+  export let name: string;
+  export let description: string;
+  export let href: string | undefined = undefined;
+  export let logo: string | undefined = undefined;
+</script>
+
+<div class="flex flex-col gap-1 rounded-lg border-2 border-neutral-700 bg-neutral-800 p-4">
+  <div class="flex flex-row items-center justify-between">
+    <div class="flex flex-row items-center gap-2">
+      {#if logo}
+        <img class="w-5" src={logo} alt="Essential Logo" />
+      {/if}
+
+      <h5 class="text-lg font-medium text-neutral-200">{name}</h5>
+    </div>
+
+    {#if href}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        class="rounded-full p-1 text-neutral-200 transition-colors hover:bg-neutral-700"
+        {href}
+      >
+        <ArrowUpRight class="h-4 w-4" />
+      </a>
+    {/if}
+  </div>
+
+  <p class="text-neutral-400">{description}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
+  import ProjectCard from "$lib/components/ProjectCard.svelte";
+  import ArrowUpRight from "$lib/components/icons/ArrowUpRight.svelte";
   import InlineLink from "$lib/components/typography/InlineLink.svelte";
   import Title from "$lib/components/typography/Title.svelte";
 </script>
 
-<section class="flex flex-col gap-2">
-  <Title>Hi, I'm Caoimhe 👋</Title>
+<main class="flex flex-col gap-6">
+  <section class="flex flex-col gap-2">
+    <Title>Hi, I'm Caoimhe 👋</Title>
 
-  <h2 class="text-lg text-neutral-700 dark:text-neutral-300">
-    I'm a software developer and open-source contributor. I currently work at
-    <InlineLink href="https://essential.gg" external>Essential</InlineLink>
-    and code in Java, Kotlin, and TypeScript.
-  </h2>
-</section>
+    <h2 class="text-lg text-neutral-700 dark:text-neutral-300">
+      I'm a software developer and open-source contributor. I currently work at
+      <InlineLink href="https://essential.gg" external>Essential</InlineLink>
+      and code in Java, Kotlin, and TypeScript.
+    </h2>
+  </section>
+
+  <section class="grid grid-flow-col grid-cols-3">
+    <ProjectCard
+      name="Essential"
+      description="Essential is a quality of life mod that boosts Minecraft Java to the next level."
+      logo="https://static.essential.gg/icon/96x96.png"
+      href="https://essential.gg"
+    />
+  </section>
+</main>
